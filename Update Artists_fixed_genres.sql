8a292bd738b69d04368dd14edf2d7861a4483cd5
Pham Hanh <48047723+Joypham@users.noreply.github.com>
2019-04-23 17:55:02 +0700

    Update Update Artists_fixed_genres.sql

--- Update Artists_fixed_genres.sql
+++ Update Artists_fixed_genres.sql
@@ -74,6 +74,43 @@ Info ->> '$.allmusic.styles' not like '%null%'
 and
 LENGTH(info ->> '$.fixed_genres') > 12
 -- Step 2: câu lệnh update: 
+UPDATE artists 
+Join (
+Select 
+name,
+uuid,
+Info ->> '$.allmusic.styles',
+info ->> '$.fixed_genres',
+genrematch.GenreName,
+Json_remove(info,Json_unquote(Json_search(info,'all','Pop/Rock','$'))) as remove_poprock,
+info,
+ROW_NUMBER () over (PARTITION BY artists.uuid ) as `Rankovertrack`,
+LENGTH(info ->> '$.fixed_genres')
+from artists
+left Join genrematch on 
+(SUBSTRING_INDEX(Info ->> '$.allmusic.styles',',',1) = genrematch.GenreName
+or
+SUBSTRING_INDEX(Info ->> '$.allmusic.styles',',',-1) = genrematch.GenreName
+or
+Info ->> '$.allmusic.styles' like CONCAT('%',',',genrematch.GenreName,',','%'))
+and genrematch.GenreName <> 'Pop/Rock'
+where 
+info ->> '$.fixed_genres' like '%Pop/Rock%'
+and 
+Info ->> '$.allmusic.styles' is not null 
+and 
+Info ->> '$.allmusic.styles' not like '%null%'
+and
+LENGTH(info ->> '$.fixed_genres') <> 12
+-- and 
+-- artists.uuid = '00139C420D124EBDBCD86C92F7A89094'
+) as t1
+ on artists.uuid = t1.uuid
+Set artists.info = t1.remove_poprock
+
+
+
+
 
 
 
